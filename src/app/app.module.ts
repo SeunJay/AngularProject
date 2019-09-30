6df08720e85f9a452b7e9442af28a06ab76b4d79
seun jay <seunjay92@gmail.com>
2019-10-01 00:48:05 +0100

    create Banner component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { CleanNavComponent } from './clean-nav/clean-nav.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    CleanNavComponent
+    CleanNavComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
